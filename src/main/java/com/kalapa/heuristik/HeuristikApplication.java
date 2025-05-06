e64d08cb812d51910a82981a081b9fdfa341bf45
José Luis Pereira <jl24pereira@gmail.com>
2025-05-06 00:30:20 -0600

    First commit

---
+++ src/main/java/com/kalapa/heuristik/HeuristikApplication.java
@@ -0,0 +1,13 @@
+package com.kalapa.heuristik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeuristikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeuristikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalapa/heuristik/HeuristikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalapa.heuristik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeuristikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
